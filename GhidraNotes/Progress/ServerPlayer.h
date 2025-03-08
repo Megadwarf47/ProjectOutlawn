6d1fb3b62c66d779b20039461bcdfcd94fa82ec9
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-03-08 13:27:35 +0300

    Update ServerPlayer.h

--- GhidraNotes/Progress/ServerPlayer.h
+++ GhidraNotes/Progress/ServerPlayer.h
@@ -2,7 +2,7 @@ namespace fb {
     class ServerPlayerChangeChatChannelMessage : public Message {
     public:
         ServerPlayer* m_player;
-        long long m_newMask;
+        uint64_t m_newMask;
     };
 }
 
@@ -12,4 +12,93 @@ namespace fb {
         ServerPlayer* m_player;
     };
 }
+
+namespace fb {
+    class ServerPlayerChangedVoiceChannelMessage : public Message {
+    public:
+        ServerPlayer* m_player;
+    };
+}
+
+namespace fb {
+    class ServerPlayerCreateMessage : public Message {
+    public:
+        ServerPlayer* m_player;
+    };
+}
+
+namespace fb {
+    class ServerPlayerCreatedForConnectionMessage : public Message {
+    public:
+        ServerConnection * m_connection;
+        ServerPlayer* m_player;
+    };
+}
+
+namespace fb {
+    class ServerPlayerDestroyMessage : public Message {
+    public:
+        ServerPlayer* m_player;
+    };
+}
+
+namespace fb {
+    class ServerPlayerEnterEntryMessage : public Message {
+    public:
+        ServerPlayer* m_player;
+        ServerEntryComponent* m_newEntry;
+        ServerEntryComponent* m_oldEntry;
+        int m_playerSide;
+        int m_playerId;
+        bool m_hiddenObject;
+    };
+}
+
+namespace fb {
+    class ServerPlayerEnteredLevelMessage : public Message {
+    public:
+        ServerPlayer* m_player;
+        bool* m_isLoadingSave;
+    };
+}
+
+namespace fb {
+    class ServerPlayerExitEntryMessage : public Message {
+    public:
+        ServerPlayer* m_player;
+        ServerControllableEntity* m_controllable;
+        ServerEntryComponent* m_entry;
+    };
+}
+
+namespace fb {
+    class ServerPlayerInstantSuicideMessage : public Message {
+    public:
+        ServerPlayer* m_player;
+        ServerDamageGiverInfo* m_damageGiverInfo;
+    };
+}
+
+namespace fb {
+    class ServerPlayerKilledMessage : public Message {
+    public:
+        ServerPlayer* m_victim;
+        ServerDamageGiverInfo* m_damageGiverInfo;
+        ServerPlayer* m_inflictor;
+        char* m_weaponName;
+        Vec3 m_position;
+        ServerEntryComponent* m_oldEntry;
+        bool m_roadKill;
+        bool m_headShot;
+        bool m_victimInReviveState;
+    };
+}
+
+
+
+
+
+
+
+
 
