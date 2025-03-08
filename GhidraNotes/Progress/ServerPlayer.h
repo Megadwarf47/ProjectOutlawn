a63425695b5e419e4defece7f5b17c8a918fbd3c
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-03-08 11:50:43 +0300

    Update ServerPlayer.h

--- GhidraNotes/Progress/ServerPlayer.h
+++ GhidraNotes/Progress/ServerPlayer.h
@@ -1,1 +1,7 @@
-a
+class ServerPlayerCreateMessage : public fb::Message {
+    public:
+        typedef void* (__thiscall* newInstance_t)(fb:: ServerPlayerCreateMessage* th);
+        static newInstance_t newInstance;
+    public:
+        fb::ServerPlayer* m_player;
+    };
