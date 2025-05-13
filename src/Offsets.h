06c5ae001662267def5540d316ebaa38c86c432d
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-05-13 16:42:35 +0300

    Update Offsets.h

--- src/Offsets.h
+++ src/Offsets.h
@@ -1,29 +1,29 @@
 //Offsets by Silkker
 
 //DEDI SERVER
-0x14000B070 => Main::Init
-0x140011AE0 => Main::SpawnServer
-0x14000AFB0 => GameLoopInfo::Init
+Main::Init => 0x14000B070
+Main::SpawnServer => 0x140011AE0
+GameLoopInfo::Init => 0x14000AFB0
 
-0x1421EFE10 => BFSERVERSETTINGS // Not sure about this
-0x141C472E8 => BFDedicatedServerSettings // Not sure about this
+BFSERVERSETTINGS => 0x1421EFE10 // Not sure about this
+BFDedicatedServerSettings => 0x141C472E8 // Not sure about this
 
 //MULTI PURPOSE
-0x140767A60 => ServerPlayer::SetTeamId
-0x1408038F0 => EnginePeer::Init
-0x141464B10 => LevelSetup::LevelSetup
-0x1404FA7F0 => LevelSetup::SetInclusionOption
-0x1407056D0 => ServerConnection::KickPlayer
-0x14075CDC0 => ServerPlayerManager::CreatePlayer
-0x1403BCEA0 => SettingsManager::GetSettingsObject
+ServerPlayer::SetTeamId => 0x140767A60
+EnginePeer::Init => 0x1408038F0
+LevelSetup::LevelSetup => 0x141464B10
+LevelSetup::SetInclusionOption => 0x1404FA7F0
+ServerConnection::KickPlayer => 0x1407056D0
+ServerPlayerManager::CreatePlayer => 0x14075CDC0
+SettingsManager::GetSettingsObject => 0x1403BCEA0
 
 //CLIENT
-0x140559CD0 => Client::ConnectToAddress
-0x14053F180 => Client::StateChange
+Client::ConnectToAddress => 0x140559CD0
+Client::StateChange => 0x14053F180
 
 //FOR DEBUGGING
-0x14035AD00 => MessaageManager::DispatchMessage
-0x14055D3B0 => OnlineManager::OnGotDisconnected
+MessaageManager::DispatchMessage => 0x14035AD00
+OnlineManager::OnGotDisconnected => 0x14055D3B0
 
 
 //Offsets to find (Not sure if all of these exist)
@@ -33,16 +33,16 @@
 ? => CLIENT_STATE_CHANGE
 ? => CLIENT_INIT_NETWORK
 
-? => GET_CLIENT_INSTANCE
-? => GET_SETTINGS_OBJECT
+GET_CLIENT_INSTANCE => ?
+GET_SETTINGS_OBJECT => ?
 
-? => SERVER_START
-? => SERVER_CONSTRUCTOR
-? => SERVER_PATCH
-? => SERVER_PATCH2
-? => SERVERPLAYERMANAGER
-? => SERVERGAMECONTEXT_INSTANCE
+SERVER_START => ?
+SERVER_CONSTRUCTOR => ?
+SERVER_PATCH => ?
+SERVER_PATCH2 => ?
+SERVERPLAYERMANAGER => ?
+SERVERGAMECONTEXT_INSTANCE => ?
 
-? => GLOBAL_CLIENT
-? => GLOBAL_SETTINGS_MANAGER
-? => EXECUTIONCONTEXT_GETNATIVEDATAPATH
+GLOBAL_CLIENT => ?
+GLOBAL_SETTINGS_MANAGER => ?
+EXECUTIONCONTEXT_GETNATIVEDATAPATH => ?
