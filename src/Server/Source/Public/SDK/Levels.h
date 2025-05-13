f5e9182ac1e9cc90f0c6bb844db40e7b08b26f42
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-05-13 10:45:01 +0300

    Update Levels.h

--- src/Server/Source/Public/SDK/Levels.h
+++ src/Server/Source/Public/SDK/Levels.h
@@ -13,20 +13,18 @@ struct GameLevel
 };
 
 static GameLevel s_game_levels[] = {
-    { "Levels/Coastal/Level_COOP_Coastal/Level_COOP_Coastal", "Driftwood Shores (Ops)" },
-    { "Levels/Coastal/Rush_Coastal_001/Rush_Coastal_001", "Driftwood Shores" },
-    { "Levels/COOP_Suburbia/Level_COOP_Suburbia/Level_COOP_Suburbia", "Level_COOP_Suburbia" },
-    { "Levels/Desert/Level_COOP_Desert/Level_COOP_Desert", "Level_COOP_Desert" },
-    { "Levels/Desert/Level_COOP_Palms/Level_COOP_Palms", "Crash Course" },
-    { "Levels/Desert/Level_COOP_001/Level_COOP_001", "Level_COOP_001" },
-    { "Levels/Level_Frontend_Gen3/Level_Frontend_Gen3", "Level_Frontend_Gen3" },
-    { "Levels/Mainstreet/Level_COOP_Mainstreet/Level_COOP_Mainstreet", "Level_COOP_Mainstreet" },
-    { "Levels/Sandbox/Level_Arena_Sandbox/Level_Arena_Sandbox", "Level_Arena_Sandbox" },
-    { "Levels/Sandbox/Level_COOP_Sandbox/Level_COOP_Sandbox", "Level_COOP_Sandbox" },
-    { "Levels/Sandbox/Level_Dylan_Mainstreet/Level_Dylan_Mainstreet", "Dylan_Mainstreet" },
-    { "Levels/Sandbox/Level_Shawbs_Sandbox/Level_Shawbs_Sandbox", "Shawbs_Sandbox" },
-    { "Levels/Sandbox/Level_Suburbia2/Level_Suburbia2", "Main Street" },
-    { "Levels/Sandbox/Level_SuburbiaC/Level_SuburbiaC", "SuburbiaC" },
+    { "_pvz/Levels/Coastal/Level_COOP_Coastal/Level_COOP_Coastal", "SHARKBITE SHORES" },
+    { "_pvz/Levels/Coastal/Level_COOP_Coastal/Level_COOP_Coastal", "PORT SCALLYWAG" },
+    { "_pvz/Levels/Desert/Level_COOP_Desert/Level_COOP_Desert", "JEWEL JUNCTION" },
+    { "_pvz/Levels/Desert/Level_COOP_Palms/Level_COOP_Palms", "CRASH COURSE" },
+    { "_pvz/Levels/Mainstreet/Level_COOP_Mainstreet/Level_COOP_Mainstreet", "GARDEN CENTER" },
+    { "_pvz/Levels/Mainstreet/Level_COOP_Mainstreet/Level_COOP_Mainstreet", "CHOMP TOWN" },
+    { "_pvz/Levels/COOP_Suburbia/Level_COOP_Suburbia/Level_COOP_Suburbia", "SUBURBAN FLATS" },
+    { "_pvz/Levels/COOP_Suburbia/Level_COOP_Suburbia/Level_COOP_Suburbia", "ZOMBOSS ESTATE" }, //Not sure
+    { "_pvz/Levels/Sandbox/Level_SuburbiaC/Level_SuburbiaC", "WALL-NUT HILLS" },
+    { "_pvz/Levels/Sandbox/Level_Suburbia2/Level_Suburbia2", "MAIN STREET" },
+    { "_pvz/Levels/Coastal/Rush_Coastal_001/Rush_Coastal_001", "DRIFTWOOD SHORES" },
+    { "_pvz/Levels/Desert/Rush_Desert_001/Rush_Desert_001", "CACTUS CANYON" },
 };
 
 GameLevel GetGameLevel(const char* level);
